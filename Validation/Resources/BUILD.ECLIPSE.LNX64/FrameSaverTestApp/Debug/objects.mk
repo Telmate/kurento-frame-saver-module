dbdb5f0b8b0caec3602769f6b6f36cb43c01f148
jbendor-dsc <rodneb@retet.net>
2016-11-15 08:14:33 -0800

    1. moved all "makefiles" to project-specific folders under "Validation/Resources"
    Signed-off-by: jbendor-dsc <rodneb@retet.net>

--- Validation/Resources/BUILD.ECLIPSE.LNX64/FrameSaverTestApp/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lFrameSaverFilterLib.Debug -lpng -lz -lm -lpthread -lglib-2.0 -lgstapp-1.5 -lgobject-2.0 -lgstreamer-1.5 -lgstreamer_utils -lgstreamer_tools
-
