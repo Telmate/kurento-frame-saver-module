f317039906403b5454b30f0c15ea2242e01ec718
jbendor-dsc <rodneb@retet.net>
2016-11-20 20:15:49 -0800

    1. created separate project for building the FrameSaverPlugin for Gstreamer-1.5
    Signed-off-by: jbendor-dsc <rodneb@retet.net>

---
+++ Validation/Resources/BUILD.ECLIPSE.LNX64/GstFrameSaverPluginLib/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lFrameSaverFilterLib-Debug -lpng12 -lz -lm -lpthread -lglib-2.0 -lgstapp-1.5 -lgobject-2.0 -lgstreamer-1.5
+
---
+++ Validation/Resources/BUILD.ECLIPSE.LNX64/GstFrameSaverPluginLib/Release/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/gst_Frame_Saver_Plugin.c 
+
+OBJS += \
+./src/gst_Frame_Saver_Plugin.o 
+
+C_DEPS += \
+./src/gst_Frame_Saver_Plugin.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -std=c99 -D_LINUX -D_DEBUG -D_LIB_SHARED -D_IS_LIB_FOR_PLUGIN_ -D_USING_PNG_LIBRARY_ -D_USING_SINKER_CAPS_ -I"/home/jonny/Work/TELMATE/GST/ECLIPSE.LNX64.GST150/GstFrameSaverPluginLib/src" -I/usr/include/gstreamer-1.5 -I/usr/lib/x86_64-linux-gnu/gstreamer-1.5/include -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/glib-2.0 -O3 -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
