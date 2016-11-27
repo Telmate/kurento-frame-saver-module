900ed46a335345c967b373efa34403a1666df75f
jbendor-dsc <rodneb@retet.net>
2016-11-27 15:05:17 -0800

    1. deleted binary files created by Eclipse IDE when building "FrameSaverFilterLibStatic"
    Signed-off-by: jbendor-dsc <rodneb@retet.net>

--- Validation/Resources/BUILD.ECLIPSE.LNX64/FrameSaverFilterLibStatic/Release/src/wrapped_natives.d
+++
@@ -1,4 +0,0 @@
-src/wrapped_natives.o: ../src/wrapped_natives.c \
- /home/jonny/Work/TELMATE/GST/ECLIPSE.LNX64.GST150/FrameSaverFilterLibStatic/inc/wrapped_natives.h
-
-/home/jonny/Work/TELMATE/GST/ECLIPSE.LNX64.GST150/FrameSaverFilterLibStatic/inc/wrapped_natives.h:
