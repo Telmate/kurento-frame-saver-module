637bc9a3506f5c69ce3f8a39d419239a4e565407
jbendor-dsc <rodneb@retet.net>
2016-12-03 14:36:09 -0800

    1. created new scaffold "kurento-frame-saver-video-filter" hoping to solve Kava-KMS-Exception 2. same exception as before, only with "Kurento" as prefix of the not-found remote-class
    Signed-off-by: jbendor-dsc <rodneb@retet.net>

---
+++ ModuleAutoGen-frame-saver-video-filter/kurento-frame-saver-video-filter/src/gst-plugins/framesavervideofilter/gst_Frame_Saver_Video_Filter_Plugin.h
@@ -0,0 +1,35 @@
+/*
+ * ======================================================================================
+ * File:        gst_Frame_Saver_Video_Filter_Plugin.h
+ *
+ * Purpose:     Kurento+Gstreamer plugin-filter --- uses "Frame-Saver" for full behavior.
+ *
+ * History:     1. 2016-11-25   JBendor     Created as copy of "gst_Frame_Saver_Plugin.h"
+ *              2. 2016-11-28   JBendor     Updated
+ *
+ * Copyright (c) 2016 TELMATE INC. All Rights Reserved. Proprietary and confidential.
+ *               Unauthorized copying of this file is strictly prohibited.
+ * ======================================================================================
+ */
+
+#ifndef _GST_FRAME_SAVER_VIDEO_FILTER_PLUGIN_H_
+#define _GST_FRAME_SAVER_VIDEO_FILTER_PLUGIN_H_
+
+#include <config.h>
+
+#ifdef THIS_PLUGIN_NAME
+    #undef THIS_PLUGIN_NAME
+#endif
+
+#define THIS_PLUGIN_NAME  "FrameSaverVideoFilterPlugin"
+
+#define  _SAVE_IMAGE_FRAMES_
+
+#define  _VIDEO_FILTER_TYPE_
+
+#define  NO___PROVIDE_APPSINK_STUBS_
+
+#define  NO___ALLOW_DYNAMIC_PARAMS__
+
+
+#endif  // _GST_FRAME_SAVER_VIDEO_FILTER_PLUGIN_H_
