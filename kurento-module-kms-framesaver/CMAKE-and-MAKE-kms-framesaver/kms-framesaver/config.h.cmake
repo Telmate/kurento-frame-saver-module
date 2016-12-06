26aeb4f5a738b26eacaf7ce9c49df772f74110e3
jbendor-dsc <rodneb@retet.net>
2016-12-06 12:57:57 -0800

    1. added some files to sub-folder "CMAKE-and-MAKE-kms-framesaver\kms-framesaver"
    Signed-off-by: jbendor-dsc <rodneb@retet.net>

---
+++ kurento-module-kms-framesaver/CMAKE-and-MAKE-kms-framesaver/kms-framesaver/config.h.cmake
@@ -0,0 +1,17 @@
+#ifndef __KMS_FRAME_SAVER_VIDEO_FILTER_CONFIG_H__
+#define __KMS_FRAME_SAVER_VIDEO_FILTER_CONFIG_H__
+
+/* Version */
+#cmakedefine VERSION "@VERSION@"
+
+/* Package name */
+#cmakedefine PACKAGE "@PACKAGE@"
+
+/* The gettext domain name */
+#cmakedefine GETTEXT_PACKAGE "@GETTEXT_PACKAGE@"
+
+/* Library installation directory */
+#cmakedefine KURENTO_MODULES_SO_DIR "@KURENTO_MODULES_SO_DIR@"
+
+#endif /* __KMS_FRAME_SAVER_VIDEO_FILTER_CONFIG_H__ */
+
